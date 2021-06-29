f92e90e609d6445b3d5845b63c390455ef393b28
Robert <mail@rbeekman.nl>
2021-06-29 22:41:40 +0200

    Logitech - Added G PowerPlay Mousepad PID

--- src/Devices/Artemis.Plugins.Devices.Logitech/LogitechDeviceProvider.cs
+++ src/Devices/Artemis.Plugins.Devices.Logitech/LogitechDeviceProvider.cs
@@ -6,6 +6,8 @@ using Artemis.Core;
 using Artemis.Core.DeviceProviders;
 using Artemis.Core.Services;
 using HidSharp;
+using RGB.NET.Core;
+using RGB.NET.Devices.Logitech;
 using Serilog;
 using Serilog.Events;
 
@@ -27,6 +29,11 @@ namespace Artemis.Plugins.Devices.Logitech
         {
             RGB.NET.Devices.Logitech.LogitechDeviceProvider.PossibleX64NativePaths.Add(Path.Combine(Plugin.Directory.FullName, "x64", "LogitechLedEnginesWrapper.dll"));
             RGB.NET.Devices.Logitech.LogitechDeviceProvider.PossibleX86NativePaths.Add(Path.Combine(Plugin.Directory.FullName, "x86", "LogitechLedEnginesWrapper.dll"));
+
+            RGB.NET.Devices.Logitech.LogitechDeviceProvider.PerZoneDeviceDefinitions.Add(
+                0xC53A, RGBDeviceType.Mousepad, "G PowerPlay Mousepad", LedMappings.ZoneMousepad, (LogitechDeviceType.Mousemat, 2)
+            );
+
             _rgbService.AddDeviceProvider(RgbDeviceProvider);
 
             if (_logger.IsEnabled(LogEventLevel.Debug))
